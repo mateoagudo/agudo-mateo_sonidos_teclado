e471525a29b03767f76a102b5a7db8346d9bcb73
mateoagudo <mateo.agudo@ucuenca.edu.ec>
2023-12-21 12:48:12 -0500

    se incluyó la librería p5 y p5 sound, se importó 6 sonidos y se controló la reproducción de los sonidos a través de la función mouseClicked, mouseDragged y keyPressed con un switch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,102 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+// let sonidoBurbuja;
+let sonidos = [];
+
+function preload() {
+  // soundFormats("wav", "mp3", "ogg");
+  // sonidoBurbuja = loadSound("sonidos/burbuja");
+  for (let i = 0; i < 7; i++) {
+    sonidos[i] = loadSound("sonidos/" + i + ".wav");
+  }
 }
 
-function draw() {
+function setup() {
+  let cnv = createCanvas(windowWidth, windowHeight);
+  cnv.mouseClicked(presionado);
+}
+
+function presionado() {
+  // sonidoBurbuja.play();
+  // circle(mouseX, mouseY, 20);
+  let r = Math.floor(random(7));
   circle(mouseX, mouseY, 20);
+
+  switch (r) {
+    case 0:
+      sonidos[0].play();
+      break;
+    case 1:
+      sonidos[1].play();
+      break;
+    case 2:
+      sonidos[2].play();
+      break;
+    case 3:
+      sonidos[3].play();
+      break;
+    case 4:
+      sonidos[4].play();
+      break;
+    case 5:
+      sonidos[5].play();
+      break;
+    case 6:
+      sonidos[6].play();
+      break;
+  }
+}
+function draw() {}
+
+function mouseDragged() {
+  let r = Math.floor(random(7));
+  circle(mouseX, mouseY, 20);
+
+  switch (r) {
+    case 0:
+      sonidos[0].play();
+      break;
+    case 1:
+      sonidos[1].play();
+      break;
+    case 2:
+      sonidos[2].play();
+      break;
+    case 3:
+      sonidos[3].play();
+      break;
+    case 4:
+      sonidos[4].play();
+      break;
+    case 5:
+      sonidos[5].play();
+      break;
+    case 6:
+      sonidos[6].play();
+      break;
+  }
+}
+
+function keyPressed() {
+  switch (key) {
+    case " ":
+      sonidos[0].play();
+      break;
+    case "s":
+      sonidos[1].play();
+      break;
+    case "d":
+      sonidos[2].play();
+      break;
+    case "f":
+      sonidos[3].play();
+      break;
+    case "g":
+      sonidos[4].play();
+      break;
+    case "h":
+      sonidos[5].play();
+      break;
+    case "j":
+      sonidos[6].play();
+      break;
+  }
 }
